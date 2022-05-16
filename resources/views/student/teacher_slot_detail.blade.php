c2dcf99d01263b94795b8a6e0feb76b1690f6295
JunelWesternVp <junel@photoup.net>
2022-05-16 13:59:35 +0800

    fix the disabled click in times status.

--- resources/views/student/teacher_slot_detail.blade.php
+++ resources/views/student/teacher_slot_detail.blade.php
@@ -49,7 +49,7 @@
                     </td>
                     @elseif($t["status_$curr_date"] =="times")
                     <td style="width: 100px" >
-                    <a href="/student/contact/{{$id}}/{{$curr_date}}/{{$t["time"]}}/{{$t["status_$curr_date"]}}"> 
+                    <a onclick="return false" href="/student/contact/{{$id}}/{{$curr_date}}/{{$t["time"]}}/{{$t["status_$curr_date"]}}"> 
                    <span class="text-danger">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
